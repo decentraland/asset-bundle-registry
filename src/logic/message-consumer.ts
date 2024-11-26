6fd2c55aa7367f49f2020997945b9d0a73ccaa7a
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2024-11-26 11:52:41 -0300

    chore: add debug log

--- src/logic/message-consumer.ts
+++ src/logic/message-consumer.ts
@@ -1,5 +1,6 @@
 import { AppComponents, MessageConsumerComponent } from '../types'
 import { sleep } from '../utils/timer'
+import { log } from 'console'
 
 export function createMessagesConsumerComponent({
   logs,
