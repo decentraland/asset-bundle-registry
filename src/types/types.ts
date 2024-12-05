543bed634d50327d38e7d602a11e69b5ca892681
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2024-12-05 11:34:10 -0300

    feat: save deployment into the database every time a deployment event arrives (#4)

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,10 +1,15 @@
-export type Registry = {
-  entityId: string
-  pointer: string
-  assetBundles: {
-    version: string
-    mac: string[]
-    windows: string[]
-    timestamp: number
-  }[]
+import { Entity } from '@dcl/schemas'
+
+export namespace Registry {
+  export type Status = {
+    status: StatusValues
+  }
+
+  export enum StatusValues {
+    PENDING = 'pending',
+    OPTMIZED = 'optimized',
+    ERROR = 'error'
+  }
+
+  export type DbEntity = Omit<Entity, 'version'> & Status
 }
