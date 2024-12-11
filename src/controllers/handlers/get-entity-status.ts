cfec3e05e710d6d40df80475b1127d16b6e65f00
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2024-12-11 11:15:17 -0300

    feat: single entity status endpoint

---
+++ src/controllers/handlers/get-entity-status.ts
@@ -0,0 +1,72 @@
+import { DecentralandSignatureContext } from '@dcl/platform-crypto-middleware'
+import { HandlerContextWithPath, Registry } from '../../types'
+import { EthAddress } from '@dcl/schemas'
+
+function isOwnedByUser(entity: Registry.DbEntity | null, userAddress: EthAddress): boolean {
+  console.log({ entity: JSON.stringify(entity), userAddress })
+  return !!entity && entity.deployer.toLocaleLowerCase() === userAddress.toLocaleLowerCase()
+}
+
+type StatusPerPlatform = {
+  mac: Registry.StatusValues
+  windows: Registry.StatusValues
+}
+
+type EntityStatusResponse = {
+  complete: boolean
+  lods: StatusPerPlatform
+  assetBundles: StatusPerPlatform
+  catalyst: 'complete' | 'pending'
+}
+
+export async function getEntityStatusHandler(
+  context: HandlerContextWithPath<'db', '/entities/status/:id'> & DecentralandSignatureContext<any>
+) {
+  const {
+    params,
+    components: { db },
+    verification
+  } = context
+
+  const entityId: string | undefined = params.id
+  const userAddress: EthAddress = verification!.auth
+
+  if (!entityId) {
+    return {
+      status: 400,
+      body: {
+        ok: false,
+        message: 'No entity id provided'
+      }
+    }
+  }
+
+  const entity = await db.getRegistryById(entityId)
+
+  if (isOwnedByUser(entity, userAddress)) {
+    return {
+      body: JSON.stringify({
+        complete: entity?.status === Registry.StatusValues.OPTMIZED,
+        // TODO: LODS status MOCKED
+        lods: {
+          mac: entity?.bundles.mac || Registry.StatusValues.PENDING,
+          windows: entity?.bundles.mac || Registry.StatusValues.PENDING
+        },
+        assetBundles: {
+          mac: entity?.bundles.mac || Registry.StatusValues.PENDING,
+          windows: entity?.bundles.windows || Registry.StatusValues.PENDING
+        },
+        // TODO: Catalyst status MOCKED
+        catalyst: 'complete'
+      } as EntityStatusResponse)
+    }
+  }
+
+  return {
+    status: 404,
+    body: {
+      ok: false,
+      message: 'No active entity found for the provided id'
+    }
+  }
+}
--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -2,12 +2,25 @@ import { Router } from '@well-known-components/http-server'
 import { GlobalContext } from '../types'
 import { getStatusHandler } from './handlers/get-service-status'
 import { getEntityHandler } from './handlers/get-entity'
+import { wellKnownComponents } from '@dcl/platform-crypto-middleware'
+import { getEntityStatusHandler } from './handlers/get-entity-status'
 
-export async function setupRouter(_: GlobalContext): Promise<Router<GlobalContext>> {
+export async function setupRouter(globalContext: GlobalContext): Promise<Router<GlobalContext>> {
   const router = new Router<GlobalContext>()
+
+  const signedFetchMiddleware = wellKnownComponents({
+    fetcher: globalContext.components.fetch,
+    optional: false,
+    onError: (err: any) => ({
+      error: err.message,
+      message: 'This endpoint requires a signed fetch request. See ADR-44.'
+    })
+  })
 
   router.get('/status', getStatusHandler)
   router.post('/entities/active', getEntityHandler)
+  router.get('/entities/status/:id', signedFetchMiddleware, getEntityStatusHandler)
+  // router.get('/entities/status', signedFetchMiddleware, getEntitiesStatusHandler)
 
   return router
 }
