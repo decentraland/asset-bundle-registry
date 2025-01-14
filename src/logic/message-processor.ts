827a03a8e3485ed13eae885340b257c3e8846d1c
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2025-01-14 10:48:33 -0300

    chore: remove try/catch statement

--- src/logic/message-processor.ts
+++ src/logic/message-processor.ts
@@ -24,25 +24,16 @@ export async function createMessageProcessorComponent({
   ]
 
   async function process(message: any) {
-    try {
-      log.debug('Processing', { message })
+    log.debug('Processing', { message })
 
-      const handlers: EventHandlerComponent[] | undefined = processors.filter((p) => p.canProcess(message))
+    const handlers: EventHandlerComponent[] | undefined = processors.filter((p) => p.canProcess(message))
 
-      if (!handlers) {
-        log.warn('No handler found for the message', { message })
-        return
-      }
-
-      await Promise.all(handlers.map((handler) => handler.process(message)))
-    } catch (error: any) {
-      log.error('Error processing message', {
-        error: error?.message || 'Unknown error',
-        stack: JSON.stringify(error?.stack) || 'Unknown stack'
-      })
-
-      throw error
+    if (!handlers) {
+      log.warn('No handler found for the message', { message })
+      return
     }
+
+    await Promise.all(handlers.map((handler) => handler.process(message)))
   }
 
   return { process }
