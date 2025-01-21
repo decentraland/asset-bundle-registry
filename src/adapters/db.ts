ae7d835ee9915169e02f8cf90c828509bc89e5cd
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2025-01-21 16:10:24 -0300

    fix: db query update

--- src/adapters/db.ts
+++ src/adapters/db.ts
@@ -71,7 +71,7 @@ export function createDbAdapter({ pg }: Pick<AppComponents, 'pg'>): DbComponent 
           content = EXCLUDED.content,
           metadata = EXCLUDED.metadata,
           status = EXCLUDED.status,
-          bundles = EXCLUDED.bundles
+          bundles = EXCLUDED.bundles,
           deployer = CASE
             WHEN EXCLUDED.deployer != '' THEN EXCLUDED.deployer
             ELSE registries.deployer
