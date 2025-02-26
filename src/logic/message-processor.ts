2199efddbe6ba4a89e29d22db51854636aa6c927
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2025-02-26 15:12:44 -0300

    fix: prevent deployments event duplicated processing (#37)

--- src/logic/message-processor.ts
+++ src/logic/message-processor.ts
@@ -40,7 +40,6 @@ export async function createMessageProcessorComponent({
       logs,
       catalyst,
       worlds,
-      entityStatusFetcher,
       registryOrchestrator,
       queuesStatusManager
     }),
