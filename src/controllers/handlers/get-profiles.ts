6b7a811ea1d6d84e927bab78d13d76229a157dce
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2025-12-15 10:44:36 -0300

    feat: align POST /profiles endpoint to return the same DTO as Catalyst's Lambdas does

--- src/controllers/handlers/get-profiles.ts
+++ src/controllers/handlers/get-profiles.ts
@@ -1,16 +1,8 @@
-import { Entity, Profile } from '@dcl/schemas'
-import { HandlerContextWithPath, ProfileDTO } from '../../types'
+import { HandlerContextWithPath } from '../../types'
 
 export async function getProfilesHandler(
   context: HandlerContextWithPath<'profileRetriever' | 'profileSanitizer', '/profiles'>
 ) {
-  const mapToResponse = (profiles: Entity[]): ProfileDTO[] => {
-    return profileSanitizer.getProfilesWithSnapshotsAsUrls(profiles).map((profile) => ({
-      timestamp: profile.timestamp,
-      avatars: (profile.metadata as Profile).avatars
-    }))
-  }
-
   const {
     components: { profileRetriever, profileSanitizer }
   } = context
