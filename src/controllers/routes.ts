fb6a339ef595e44f095671b155dbd5313ecf128d
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2025-12-18 15:11:43 -0300

    chore: use @dcl/wkc-http-server to align KeepAlive and HeadersTimeout for HTTP/2 capabilities

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,4 +1,4 @@
-import { Router } from '@well-known-components/http-server'
+import { Router } from '@dcl/wkc-http-server'
 import { GlobalContext } from '../types'
 import { getStatusHandler } from './handlers/get-service-status'
 import { getActiveEntityHandler } from './handlers/get-active-entities'
